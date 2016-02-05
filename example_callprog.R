0123a18abd61de0662612e94e66187c95555a2f6
grathee <geetika.rathee@wur.nl>
2016-02-05 15:11:48 +0100

    example code for awcptf function of gsif package

---
+++ example_callprog.R
@@ -0,0 +1,13 @@
+sndp = 30
+sltp = 25
+clyp = 48 
+orc = 23
+bld = 1200
+cec = 12
+phihox = 6.4
+x <- AWCPTF(sndp, sltp, clyp, orc, bld, cec, phihox)
+library(GSIF)
+install.packages(c("RCurl", "XML", "rgdal", "raster", "sp", "aqp", "mda", "gstat", "plotKML", "dismo", "rJava"))
+install.packages("GSIF", repos=c("http://R-Forge.R-project.org"), type = "source")
+str(x)
+attr(x, "coef")
