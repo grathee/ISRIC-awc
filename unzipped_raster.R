716c74ba4d7b290a2e3f3f285084c14a1fadb4a0
grathee <geetika.rathee@wur.nl>
2016-02-11 11:15:09 +0100

    Performed awc with raster variables

--- unzipped_raster.R
+++ unzipped_raster.R
@@ -2,10 +2,10 @@ library(raster)
 library(GSIF)
 library(rgdal)
 
-ifolder <- '/media/user/data/AWC-IN022016/sdata'
+ifolder <- '/media/user/data/AWC-IN_022016/sdata'
 
 setwd(ifolder)
-setwd('/media/user/data/AWC-IN_022016/sdata')
+
 slt <- raster("SLTPPT_sd1_M_1km_T386.tif")
 snd <- raster("SNDPPT_sd1_M_1km_T386.tif")
 cec <- raster("CEC_sd1_M_1km_T386.tif")
@@ -14,4 +14,27 @@ ph <- raster('PHIHOX_sd1_M_1km_T386.tif')
 cly <- raster('CLYPPT_sd1_M_1km_T386.tif')
 org <- raster('ORCDRC_sd1_M_1km_T386.tif')
 
+variable_brick <- brick(snd, slt, cly, org, bld, cec, ph)
 
+df <- as.data.frame(variable_brick)
+
+summary(df)
+length(df[1,])
+length(df[,1])
+str(df[1])
+str(df[1][[1]])
+df[[1]][[14899]]
+
+#test
+i <- 1
+y <- AWCPTF(df[[1]][[i]], df[[2]][[i]], df[[3]][[i]], df[[4]][[i]], df[[5]][[i]], df[[6]][[i]], df[[7]][[i]])
+str(y)
+
+#running for each pixel .. takes long
+for (i in c(1:1440000)){
+x <- AWCPTF(df[[1]][[i]], df[[2]][[i]], df[[3]][[i]], df[[4]][[i]], df[[5]][[i]], df[[6]][[i]], df[[7]][[i]])
+awc <- rbind(awc, x)
+}																																
+str(awc)
+
+awc_raster <- rasterize(slt, slt, field = awc$AWCh1, na.rm = T)
