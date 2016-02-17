e4667c2ad12ee0754539f76cc52779e059fbe8d0
grathee <geetika.rathee@wur.nl>
2016-02-17 09:24:01 +0100

    Cropped raster analysis, data frame created but conversion of df to raster can not be done

--- unzipped_raster.R
+++ unzipped_raster.R
@@ -7,6 +7,9 @@ ifolder <- '/media/user/data/AWC-IN_022016/sdata'
 setwd(ifolder)
 
 slt <- raster("SLTPPT_sd1_M_1km_T386.tif")
+plot(slt)
+Ext <- drawExtent()
+
 snd <- raster("SNDPPT_sd1_M_1km_T386.tif")
 cec <- raster("CEC_sd1_M_1km_T386.tif")
 bld <- raster('BLD_sd1_M_1km_T386.tif')
@@ -16,7 +19,8 @@ org <- raster('ORCDRC_sd1_M_1km_T386.tif')
 
 variable_brick <- brick(snd, slt, cly, org, bld, cec, ph)
 
-df <- as.data.frame(variable_brick)
+brick_crop <- crop(variable_brick, Ext)
+df <- as.data.frame(brick_crop)
 
 summary(df)
 length(df[1,])
@@ -26,15 +30,17 @@ str(df[1][[1]])
 df[[1]][[14899]]
 
 #test
-i <- 1
-y <- AWCPTF(df[[1]][[i]], df[[2]][[i]], df[[3]][[i]], df[[4]][[i]], df[[5]][[i]], df[[6]][[i]], df[[7]][[i]])
-str(y)
+# i <- 1567
+# y <- AWCPTF(df[[1]][[i]], df[[2]][[i]], df[[3]][[i]], df[[4]][[i]], df[[5]][[i]], df[[6]][[i]], df[[7]][[i]])
+# str(y)
 
 #running for each pixel .. takes long
-for (i in c(1:1440000)){
+awc <- c()
+for (i in c(1:49022)){
 x <- AWCPTF(df[[1]][[i]], df[[2]][[i]], df[[3]][[i]], df[[4]][[i]], df[[5]][[i]], df[[6]][[i]], df[[7]][[i]])
 awc <- rbind(awc, x)
-}																																
+}
 str(awc)
-
-awc_raster <- rasterize(slt, slt, field = awc$AWCh1, na.rm = T)
+awc$AWCh1
+awc_raster <- rasterize(slt_crop, slt_crop, field = awc$AWCh1, na.rm = T)
+print (awc_raster)
